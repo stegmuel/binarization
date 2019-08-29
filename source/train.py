71f300da88eb08b25332bb42199640469b8861b8
stegmuel <thomas.steg87@gmail.com>
2019-08-29 09:12:29 +0200

    modifications to accommodate training from GC

--- source/train.py
+++ source/train.py
@@ -3,6 +3,7 @@ from classes import *
 from keras.optimizers import Adam
 import keras.backend as K
 from keras.models import load_model
+import zipfile
 
 
 def jaccard_accuracy(y_true, y_pred):
@@ -28,38 +29,45 @@ def dice_loss(y_true, y_pred):
 
 
 if __name__ == '__main__':
-    train_data_dir = '../data/train_data/'
-    file_path = '../data/train_data.lst'
+    data_path = '../'
     models_path = '../models/'
-    images_names, images_gt_names = get_images_names(file_path)
+    training_path = os.path.join(data_path, 'training/')
 
-    train_images_names = images_names[0:100]
-    train_images_gt_names = images_gt_names[0:100]
-    validation_images_names = images_names[100:110]
-    validation_images_gt_names = images_gt_names[100:110]
+    # Unzip training data
+    if os.path.exists(training_path):
+        shutil.rmtree(training_path)
+    os.mkdir(training_path)
 
-    train_generator = DataGenerator(train_images_names, train_images_gt_names, 10, train_data_dir)
-    validation_generator = DataGenerator(validation_images_names, validation_images_gt_names, 10, train_data_dir)
+    with zipfile.ZipFile(os.path.join(data_path, 'training.zip'), 'r') as zip_ref:
+        zip_ref.extractall(training_path)
 
-    prepare_training(train_data_dir, file_path, 1000, ratio=0.9)
-    # if os.path.exists(os.path.join(models_path, 'UNet.h5')):
-    #     UNet = load_model(os.path.join(models_path, 'UNet.h5'),custom_objects={'jaccard_loss': jaccard_loss,
-    #                                                                            'jaccard_accuracy': jaccard_accuracy,
-    #                                                                            'dice_loss': dice_loss,
-    #                                                                            'dice_accuracy': dice_accuracy})
-    # else:
-    #     UNet = unet()
-    #     UNet.compile(optimizer=Adam(), loss=jaccard_loss, metrics=['accuracy', jaccard_accuracy, dice_accuracy])
+    train_images_names, train_images_gt_names = get_images_names(os.path.join(training_path, 'train.lst'))
+    validation_images_names, validation_images_gt_names = \
+        get_images_names(os.path.join(training_path, 'validation.lst'))
 
-    #
-    # UNet.fit_generator(
-    #     generator=train_generator,
-    #     steps_per_epoch=train_generator.__len__(),  # Compatibility with old Keras versions.
-    #     validation_data=validation_generator,
-    #     validation_steps=validation_generator.__len__(),  # Compatibility with old Keras versions.
-    #     epochs=1,
-    #     shuffle=True,
-    #     use_multiprocessing=True,
-    #     workers=4,
-    # )
-    # UNet.save(os.path.join(models_path, 'UNet.h5'))
+    train_generator = DataGenerator(train_images_names, train_images_gt_names, 10,
+                                    os.path.join(data_path, 'training/train/'))
+    validation_generator = DataGenerator(validation_images_names, validation_images_gt_names, 10,
+                                         os.path.join(data_path, 'training/validation'))
+
+    if os.path.exists(os.path.join(models_path, 'UNet.h5')):
+        UNet = load_model(os.path.join(models_path, 'UNet.h5'),custom_objects={'jaccard_loss': jaccard_loss,
+                                                                               'jaccard_accuracy': jaccard_accuracy,
+                                                                               'dice_loss': dice_loss,
+                                                                               'dice_accuracy': dice_accuracy})
+    else:
+        UNet = unet()
+        UNet.compile(optimizer=Adam(), loss=jaccard_loss, metrics=['accuracy', jaccard_accuracy, dice_accuracy])
+
+
+    UNet.fit_generator(
+        generator=train_generator,
+        steps_per_epoch=train_generator.__len__(),  # Compatibility with old Keras versions.
+        validation_data=validation_generator,
+        validation_steps=validation_generator.__len__(),  # Compatibility with old Keras versions.
+        epochs=1,
+        shuffle=True,
+        use_multiprocessing=True,
+        workers=4,
+    )
+    UNet.save(os.path.join(models_path, 'UNet.h5'))
