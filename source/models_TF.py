7bc0e5c8625c8ed501b0345acac98b4977490a6a
stegmuel <thomas.steg87@gmail.com>
2019-09-03 17:44:26 +0200

    Completed working TF model.

--- source/models_TF.py
+++ source/models_TF.py
@@ -9,7 +9,7 @@ class UNet:
         self.create()
 
     def create(self):
-        # Downsampling.
+        # Down-sampling.
         down1, self.X = down_block(self.input, kernel_size=(3, 3), num_filters=32)
         down2, self.X = down_block(self.X, kernel_size=(3, 3), num_filters=64)
         down3, self.X = down_block(self.X, kernel_size=(3, 3), num_filters=128)
@@ -19,27 +19,33 @@ class UNet:
         # Bottleneck.
         bottleneck = conv_block(self.X, kernel_size=(3, 3), num_filters=1024)
 
-        # Upsampling.
+        # Up-sampling.
         self.X = up_block(bottleneck, down5, filters_size=(3, 3), num_filters=512)
         self.X = up_block(self.X, down4, filters_size=(3, 3), num_filters=256)
         self.X = up_block(self.X, down3, filters_size=(3, 3), num_filters=128)
         self.X = up_block(self.X, down2, filters_size=(3, 3), num_filters=64)
         self.X = up_block(self.X, down1, filters_size=(3, 3), num_filters=32)
 
-        self.X = tf.layers.conv2d(inputs=self.X, filters=1, kernel_size=(1, 1), padding='SAME', activation='relu')
+        self.X = tf.layers.conv2d(inputs=self.X, filters=1, kernel_size=(1, 1), padding='SAME', activation='sigmoid')
         self.output = self.X
 
 
 def conv_block(X, kernel_size, num_filters):
-    X = tf.layers.conv2d(inputs=X, filters=num_filters, kernel_size=kernel_size, padding='SAME', activation='relu')
+    X = tf.layers.conv2d(inputs=X, filters=num_filters, kernel_size=kernel_size,
+                         kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False), padding='SAME',
+                         activation='relu')
     X = tf.layers.batch_normalization(inputs=X, axis=3)
-    X = tf.layers.conv2d(inputs=X, filters=num_filters, kernel_size=kernel_size, padding='SAME', activation='relu')
+    X = tf.layers.conv2d(inputs=X, filters=num_filters, kernel_size=kernel_size,
+                         kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False), padding='SAME',
+                         activation='relu')
     X = tf.layers.batch_normalization(inputs=X, axis=3)
     return X
 
 
 def down_block(X, kernel_size, num_filters):
-    X = tf.layers.conv2d(inputs=X, filters=num_filters, kernel_size=kernel_size, padding='SAME', activation='relu')
+    X = tf.layers.conv2d(inputs=X, filters=num_filters, kernel_size=kernel_size,
+                         kernel_initializer=tf.contrib.layers.xavier_initializer(uniform=False), padding='SAME',
+                         activation='relu')
     pool = tf.layers.max_pooling2d(inputs=X, pool_size=(2, 2), strides=(2, 2), padding='SAME')
     return X, pool
 
--- source/train_TF.py
+++ source/train_TF.py
@@ -2,6 +2,7 @@ from models_TF import *
 import numpy as np
 from utils import *
 import decimal
+import matplotlib.pyplot as plt
 
 
 def flatten_tensor(tensor):
@@ -11,17 +12,32 @@ def flatten_tensor(tensor):
     return layer_flat
 
 
+def dice_accuracy(y_true, y_pred):
+    numerator = 2 * tf.reduce_sum(y_true * y_pred)
+    denominator = tf.reduce_sum(y_true + y_pred)
+    return numerator / denominator
+
+
+def dice_loss(y_true, y_pred):
+    return 1 - dice_accuracy(y_true, y_pred)
+
+
 def jaccard_accuracy(y_true, y_pred):
-    eps = 1.0
-    y_true_flat = flatten_tensor(y_true)
-    y_pred_flat = flatten_tensor(y_pred)
-    intersection = tf.reduce_sum(y_true_flat * y_pred_flat, axis=1)
-    union = tf.reduce_sum(y_true_flat + y_pred_flat, axis=1)
-    return tf.reduce_mean(intersection / union, axis=0)
+    numerator = tf.reduce_sum(y_true * y_pred)
+    denominator = tf.reduce_sum(y_true + y_pred) - numerator
+    return numerator / denominator
 
 
 def jaccard_loss(y_true, y_pred):
     return 1 - jaccard_accuracy(y_true, y_pred)
+
+
+def bce_accuracy(y_true, y_pred):
+    return 1 - bce_loss(y_true, y_pred)
+
+
+def bce_loss(y_true, y_pred):
+    return tf.losses.log_loss(flatten_tensor(y_true), flatten_tensor(y_pred))
 
 
 def load_images(num_images):
@@ -65,13 +81,13 @@ def load_images(num_images):
     return train_images, train_images_gt, validation_images, validation_images_gt
 
 
-def train(train_images, train_images_gt, epochs=10):
-    for epoch in range(epochs):
-        feed_dict_train = {X: train_images, y: train_images_gt}
-        session.run(optimizer, feed_dict=feed_dict_train)
-        acc = session.run(accuracy, feed_dict=feed_dict_train)
-        message = "Train accuracy at epoch {} is {}"
-        print(message.format(epoch, acc))
+# def train(train_images, train_images_gt, epochs=10):
+#     for epoch in range(epochs):
+#         feed_dict_train = {X: train_images, y: train_images_gt}
+#         session.run(optimizer, feed_dict=feed_dict_train)
+#         acc = session.run(accuracy, feed_dict=feed_dict_train)
+#         message = "Train accuracy at epoch {} is {}"
+#         print(message.format(epoch, acc))
 
 
 def train_batch(train_images, train_images_gt, validation_images, validation_images_gt, epochs, batch_size=10):
@@ -83,19 +99,26 @@ def train_batch(train_images, train_images_gt, validation_images, validation_ima
 
             feed_dict_train = {X: X_batch, y: y_batch}
             session.run(optimizer, feed_dict=feed_dict_train)
-        acc = get_test_accuracy(validation_images, validation_images_gt)
-        message = "Validation accuracy at epoch {} is {}"
-        print(message.format(epoch, acc))
+        j_acc, d_acc, b_acc = get_test_accuracy(validation_images, validation_images_gt)
+        message = "epoch: {}, jaccard: {}, dice: {}, bce: {}"
+        print(message.format(epoch, j_acc, d_acc, b_acc))
 
 
 def get_test_accuracy(validation_images, validation_images_gt):
     feed_dict_test = {X: validation_images, y: validation_images_gt}
-    acc = session.run(accuracy, feed_dict=feed_dict_test)
-    return acc
+    j_acc, d_acc, b_acc = session.run([j_accuracy, d_accuracy, b_accuracy], feed_dict=feed_dict_test)
+    return j_acc, d_acc, b_acc
+
+
+def combine_cost(y_true, y_pred):
+    j_loss = jaccard_loss(y_true, y_pred)
+    d_loss = dice_loss(y_true, y_pred)
+    b_loss = bce_loss(y_true, y_pred)
+    return 1/3 * (j_loss + d_loss + b_loss)
 
 
 if __name__ == '__main__':
-    train_images, train_images_gt, validation_images, validation_images_gt = load_images(20)
+    train_images, train_images_gt, validation_images, validation_images_gt = load_images(1000)
 
     # Define placeholder variables
     X = tf.placeholder(dtype=tf.float32, shape=[None, 128, 128, 1], name='X')
@@ -105,9 +128,13 @@ if __name__ == '__main__':
     model = UNet(X)
 
     # Define cost
-    cost = jaccard_loss(y, model.output)
+    cost = bce_loss(y, model.output)
     optimizer = tf.train.AdamOptimizer().minimize(cost)
-    accuracy = jaccard_accuracy(y, model.output)
+
+    # Define accuracy metrics
+    j_accuracy = jaccard_accuracy(y, model.output)
+    d_accuracy = dice_accuracy(y, model.output)
+    b_accuracy = bce_accuracy(y, model.output)
 
     # Add an op to initialize the variables.
     init_op = tf.global_variables_initializer()
@@ -117,14 +144,26 @@ if __name__ == '__main__':
     model_path = '../models/UNet_TF.ckpt'
 
     with tf.Session() as session:
-        session.run(init_op)
-        # saver.restore(session, model_path)
-        # print("Model restored.")
+        #session.run(init_op)
+        saver.restore(session, model_path)
+        print("Model restored.")
 
-        train_batch(train_images, train_images_gt, validation_images, validation_images_gt, epochs=10, batch_size=10)
+        train_batch(train_images, train_images_gt, validation_images, validation_images_gt, epochs=1, batch_size=10)
 
         # Save the variables to disk.
         save_path = saver.save(session, '../models/UNet_TF.ckpt')
         print("Model saved in path: %s" % save_path)
+
+        feed_dict_pred = {X: validation_images, y: validation_images_gt}
+        pred = model.output
+        image_pred = session.run(pred, feed_dict=feed_dict_pred)
+
+        image_pred = np.squeeze(image_pred[0])
+
+        fig, axes = plt.subplots(1, 2, figsize=(10, 10))
+        axes[0].imshow(image_pred)
+        axes[1].imshow(np.squeeze(validation_images_gt[0]))
+        plt.show()
+
 
 
